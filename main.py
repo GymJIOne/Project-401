b2cced133f4f830dd28a50167721945d8b6a30a4
@scralet23 <gumelar@campus.tu-berlin.de Verified>
2025-12-24 11:04:40 +0100

    newcoming

---
+++ main.py
@@ -0,0 +1,15 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def subtract(a):
+    return a - 1
+def add(a):
+    return a + 2
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    res = subtract(add(2))
+    print("the result : ", res)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
